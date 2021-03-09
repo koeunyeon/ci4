f33764be492d3668c243d255b501428ccd7d5122
고은연 <koeunyeon@gmail.com>
2021-03-09 06:12:35 +0000

    코드이그나이터4 컨트롤러 다루기 - 6 - 세션 다루기

--- src/sample/app/Controllers/Sample.php
+++ src/sample/app/Controllers/Sample.php
@@ -52,4 +52,32 @@ class Sample extends BaseController
 		$input_data = $this->request->getPost();
 		var_export($input_data);
 	}
+	
+	public function session_exist()
+	{
+		$session = session();
+		$is_session_exist = $session->member_id != null;
+		return $is_session_exist ? "세션 값이 존재합니다." : "세션 값이 없습니다.";
+	}
+
+	public function session_set()
+	{
+		$session = session();
+		$session->member_id = "3";
+		return "세션이 설정되었습니다.";
+	}
+
+	public function session_get()
+	{
+		$session = session();
+		$session_value = $session->member_id;
+		return  $session_value === null ? "세션 값이 없습니다." : "세션 값은 $session_value 입니다." ;
+	}
+
+	public function session_remove()
+	{
+		$session = session();
+		$session->remove('member_id');
+		return "세션값이 삭제되었습니다.";
+	}
 }
