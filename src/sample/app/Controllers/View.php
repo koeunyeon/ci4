f4997f99e0fc7e602bf67c639fd8bf2406392d1e
고은연 <koeunyeon@gmail.com>
2021-03-15 04:25:09 +0000

    코드이그나이터4 뷰 다루기 - 3 - 체크박스

--- src/sample/app/Controllers/View.php
+++ src/sample/app/Controllers/View.php
@@ -6,5 +6,26 @@ namespace App\Controllers;
 
 class View extends BaseController
 {
-    
+   public function checkbox()
+	{    
+		$sports_data = [
+			"baseball" => "야구",
+			"soccer" => "축구",
+			"basketball" => "농구",
+		];
+
+		$sports_like = $this->request->getPost("sports_like") != null;
+		$sports_check = $this->request->getPost("sports_name");
+		if ($sports_check == null || is_array($sports_check) === false){
+			$sports_check = [];
+		}
+
+		return View("/view/checkbox",
+			[
+				"sports_like" => $sports_like,
+				"sports_data" => $sports_data,
+				"sports_check" => $sports_check
+			]
+		);
+	} 
 }
