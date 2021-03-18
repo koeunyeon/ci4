4c071325f994a7a58ca476043db520967c6242ae
고은연 <koeunyeon@gmail.com>
2021-03-18 01:33:25 +0000

    코드이그나이터4 뷰 다루기 - 6 - 텍스트 상자

--- src/sample/app/Controllers/View.php
+++ src/sample/app/Controllers/View.php
@@ -6,5 +6,9 @@ namespace App\Controllers;
 
 class View extends BaseController
 {
-    
+    public function text(): String
+	{
+		$age = $this->request->getPost("age") ?? "";
+		return View("/view/text", ['age'=>$age]);
+	}
 }
---
+++ src/sample/app/Views/view/text.php
@@ -0,0 +1,4 @@
+<form method="POST">
+나이 : <input type="text" name="age" value="<?= $age ?>" />  <!-- (1) -->
+<p><input type="submit" value="확인"/></p>
+</form>
