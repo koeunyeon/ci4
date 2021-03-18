8963ca0736583a683b02ecbfd1200a4c1d1bc4cf
고은연 <koeunyeon@gmail.com>
2021-03-18 01:43:36 +0000

    코드이그나이터4 뷰 다루기 - 7 - 비밀번호 상자

--- src/sample/app/Controllers/View.php
+++ src/sample/app/Controllers/View.php
@@ -6,5 +6,10 @@ namespace App\Controllers;
 
 class View extends BaseController
 {
-    
+    public function pw(): String
+	{
+		$input_pw = $this->request->getPost("input_pw") ?? "";
+
+		return View("/view/pw", ['input_pw'=>$input_pw]);
+	}
 }
---
+++ src/sample/app/Views/view/pw.php
@@ -0,0 +1,5 @@
+<form method="POST">
+비밀번호 : <input type="password" name="input_pw" value="<?= $input_pw ?>" />
+<p><input type="submit" value="확인"/></p>
+</form>
+입력한 비밀번호는 <span><?= $input_pw ?></span> 입니다.
