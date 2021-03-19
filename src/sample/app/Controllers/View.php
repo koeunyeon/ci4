0fd2c8bcaebd3fd5847839c9dbc84f7e310cf10b
고은연 <koeunyeon@gmail.com>
2021-03-19 22:26:46 +0000

    코드이그나이터4 뷰 다루기 - 8 - 여러 줄 텍스트 입력(textarea)

--- src/sample/app/Controllers/View.php
+++ src/sample/app/Controllers/View.php
@@ -6,5 +6,10 @@ namespace App\Controllers;
 
 class View extends BaseController
 {
-    
+    public function textarea(): String
+	{
+		$input = $this->request->getPost("input") ?? "";
+
+		return View("/view/textarea", ['input'=>$input]);
+	}
 }
---
+++ src/sample/app/Views/view/textarea.php
@@ -0,0 +1,4 @@
+<form method="POST">
+    <textarea name="input"><?= $input ?></textarea>
+<p><input type="submit" value="확인"/></p>
+</form>
