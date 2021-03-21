f2ab0717d56dc4e10309c7f1495029945c9d96bd
고은연 <koeunyeon@gmail.com>
2021-03-21 04:23:42 +0000

    코드이그나이터4 뷰 다루기 - 9 - 테이블

--- src/sample/app/Controllers/View.php
+++ src/sample/app/Controllers/View.php
@@ -6,5 +6,14 @@ namespace App\Controllers;
 
 class View extends BaseController
 {
-    
+    public function table(): string
+	{
+		$table_data = [
+			["name" => "코드이그나이터", "age" => 10, "gender" => "male"],
+			["name" => "라라벨", "age" => 25, "gender" => "female"],
+			["name" => "스프링", "age" => 76, "gender" => "unknown"],
+		];
+
+		return View("/view/table", ['table_data' => $table_data]);
+	}
 }
