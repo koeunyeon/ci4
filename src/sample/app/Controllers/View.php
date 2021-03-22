e6f6eb4e4357d01def0d8b7ef1693cba873f136e
고은연 <koeunyeon@gmail.com>
2021-03-22 01:30:49 +0000

    코드이그나이터4 뷰 다루기 - 10 - 링크

--- src/sample/app/Controllers/View.php
+++ src/sample/app/Controllers/View.php
@@ -6,5 +6,16 @@ namespace App\Controllers;
 
 class View extends BaseController
 {
+	public function link(): string
+	{
+		$link_data = [
+			['url' => 'https://koeunyeon.github.io', 'message' => '고은연 github', 'is_new_tab' => true],
+			['url' => 'https://github.com', 'message' => '깃헙', 'is_new_tab' => false],
+			['url' => 'https://velog.io/@koeunyeon', 'message' => '고은연 블로그', 'is_new_tab' => true],        
+			['url' => 'http://ci4doc.cikorea.net/', 'message' => '코드이그나이터4 한글 메뉴얼', 'is_new_tab' => true]
+		];
+
+		return View("/view/link", ['link_data' => $link_data]);
+	}
     
 }
