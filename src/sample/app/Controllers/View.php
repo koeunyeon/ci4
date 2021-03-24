a1eb812d9fa46f7cda1048450d55e00314837ec3
고은연 <koeunyeon@gmail.com>
2021-03-24 02:03:13 +0000

    코드이그나이터4 뷰 다루기 - 12 - 첨부파일

--- src/sample/app/Controllers/View.php
+++ src/sample/app/Controllers/View.php
@@ -6,5 +6,39 @@ namespace App\Controllers;
 
 class View extends BaseController
 {
-    
+	public function upload(): string
+	{
+		$file = $this->request->getFile("single_file");
+
+		$fileInfo = [];
+		if ($file != null) {
+			if (!$file->isValid()) {
+				$errorString = $file->getErrorString();
+				$errorCode = $file->getError();
+
+				$fileInfo['hasError'] = true;
+				$fileInfo['errorString'] = $errorString;
+				$fileInfo['errorCode'] = $errorCode;
+
+			} else {
+				$fileInfo['hasError'] = false;
+				if ($file->hasMoved() === false) {
+					$fileInfo['mimeType'] = $file->getMimeType();
+
+					$savedPath = $file->store();
+
+					$fileInfo['savedPath'] = $savedPath;
+					$fileInfo['clientName'] = $file->getClientName();
+					$fileInfo['name'] = $file->getName();
+					$fileInfo['clientMimeType'] = $file->getClientMimeType();
+					$fileInfo['clientExtention'] = $file->getClientExtension();
+					$fileInfo['guessExtention'] = $file->guessExtension();
+				}
+			}
+		}
+
+		return View("/view/upload", [
+			'fileInfo' => $fileInfo
+		]);
+	}    
 }
