5e22d967198531232605b84f4109284f7f93d52b
고은연 <koeunyeon@gmail.com>
2021-03-25 02:18:11 +0000

    코드이그나이터4 뷰 다루기 - 13 - 첨부파일 여러개 업로드

--- src/sample/app/Controllers/View.php
+++ src/sample/app/Controllers/View.php
@@ -6,5 +6,50 @@ namespace App\Controllers;
 
 class View extends BaseController
 {
-    
+	public function upload_multi(): string
+	{
+		$files = $this->request->getFileMultiple("files");
+
+		if ($files == null) {
+			return View("/view/upload_multi", [
+				'file_info_array' => []
+			]);
+		}
+
+		$file_info_array = [];
+
+		foreach ($files as $file) {
+			$fileInfo = [];
+			if ($file != null) {
+				if (!$file->isValid()) {
+					$errorString = $file->getErrorString();
+					$errorCode = $file->getError();
+					$fileInfo['hasError'] = true;
+					$fileInfo['errorString'] = $errorString;
+					$fileInfo['errorCode'] = $errorCode;
+
+				} else {
+					$fileInfo['hasError'] = false;
+					if ($file->hasMoved() === false) {                    
+						$fileInfo['mimeType'] = $file->getMimeType();
+
+						$savedPath = $file->store();
+
+						$fileInfo['savedPath'] = $savedPath;
+						$fileInfo['clientName'] = $file->getClientName();
+						$fileInfo['name'] = $file->getName();
+						$fileInfo['clientMimeType'] = $file->getClientMimeType();
+						$fileInfo['clientExtention'] = $file->getClientExtension();
+						$fileInfo['guessExtention'] = $file->guessExtension();
+					}
+				}
+			}
+
+			array_push($file_info_array, $fileInfo);
+		}
+
+		return View("/view/upload_multi", [
+			'file_info_array' => $file_info_array
+		]);
+	}    
 }
