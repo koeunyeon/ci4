b1c6bb8752bfb4b2c89db801d7273dade5f2a7ce
고은연 <koeunyeon@gmail.com>
2021-03-29 05:04:52 +0000

    코드이그나이터4 데이터베이스 다루기 - 3 - 모델의 정의와 기본 사용법 데이터 읽기

--- src/sample/app/Controllers/Model.php
+++ src/sample/app/Controllers/Model.php
@@ -25,4 +25,37 @@ class Model extends BaseController
             return $e->getMessage();
         }
     }
+	
+	public function readall(): \CodeIgniter\HTTP\Response
+	{
+		$sampleModel = new SampleModel();
+		$data = $sampleModel
+			->where('name', 'ci4')
+			->findAll();
+
+		$last_query = $sampleModel->db->getLastQuery();
+		error_log(print_r($last_query, true));
+
+		return $this->response->setJSON($data);
+	}
+
+	public function readfirst(): \CodeIgniter\HTTP\Response
+	{
+		$sampleModel = new SampleModel();
+		$data = $sampleModel
+				->where(['name'=>'ci4'])
+				->orderBy('sample_id', 'desc')
+				->first();
+
+		error_log($sampleModel->db->showLastQuery());
+
+
+		return $this->response->setJSON($data);
+	}
+
+	public function find(){
+		$sampleModel = new SampleModel();
+		$data = $sampleModel->find(1);
+		return $this->response->setJSON($data);
+	}
 }
