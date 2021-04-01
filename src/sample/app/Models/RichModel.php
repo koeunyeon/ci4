9ee5383b1aafa1bb3c74d756617e32f60c81b75e
고은연 <koeunyeon@gmail.com>
2021-04-01 00:56:25 +0000

    코드이그나이터4 데이터베이스 다루기 - 6 - 모델 유효성 검사

--- src/sample/app/Models/RichModel.php
+++ src/sample/app/Models/RichModel.php
@@ -12,6 +12,26 @@ class RichModel  extends Model
     protected $allowedFields = ['name','age'];
     protected $primaryKey = "sample_rich_id";
 
-    protected $useSoftDeletes = true; // (1)
-    protected $useTimestamps = true; // (2)
+    protected $useSoftDeletes = true;
+    protected $useTimestamps = true;
+	
+	// 유효성 검사 규칙.
+    protected $validationRules = [
+        'name' => 'required|min_length[4]|max_length[10]',
+        'age' => 'required|is_natural|less_than[150]',
+    ];
+
+    // 유효성 검사 실패시 에러 메세지.
+    protected $validationMessages = [
+        'name' => [
+            'required' => '이름이 필요합니다',
+            'min_length' => '이름은 최소 4글자 이상입니다.',
+            'max_length' => '이름은 최대 10글자 이하입니다.'
+        ],
+        'age' => [
+            'required' => '필수값입니다',
+            'is_natural' => "나이는 자연수여야 합니다.",
+            'less_than' => "정말 150세 이상이신가요?"
+        ]
+    ];
 }
