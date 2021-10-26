9747e80b21cae901e7a86e98fbf5750ddb449674
koeunyeon <koeunyeon@gmail.com>
2021-10-26 11:43:50 +0900

    Create PostService.php

---
+++ src/sample/app/Services/PostService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+
+use App\Entities\PostEntity;
+use App\Models\PostsModel;
+
+class PostService  // (1)
+{
+    public function create($post_data, $memberId)  // (2)
+    {
+        $postEntity = new PostEntity(); // (3)
+        $postEntity->fill($post_data); // (4)
+        $postEntity->author = $memberId; // (5)
+
+        $postModel = new PostsModel(); // (6)
+        $post_id = $postModel->insert($postEntity); // (7)
+
+        if ($post_id) {
+            return [true, $post_id, []]; // (8)
+        }
+
+        return [false, null, $postModel->errors()]; // (9)
+    }
+
+    private static $postService = null; // (10)
+
+    public static function factory() // (11)
+    {
+        if (self::$postService === null) { // (12)
+            self::$postService = new PostService();
+        }
+
+        return self::$postService; // (13)
+    }
+}
