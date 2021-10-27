7a01f002fcf752716b45ed25ae908a3007a045c1
koeunyeon <koeunyeon@gmail.com>
2021-10-27 16:38:22 +0900

    글 생성 뷰 바꾸기

--- src/sample/app/Views/post/create.php
+++ src/sample/app/Views/post/create.php
@@ -6,15 +6,15 @@
 <form method="POST">
     <div class="form-group"> <!-- (1) -->
         <label for="title">제목을 알려주세요</label>  <!-- (2) -->
-        <input type="text" class="form-control" id="title" name="title" value="<?= $post_data['title'] ?? "" ?>" placeholder="제목" required />  <!-- (3) -->
+        <input type="text" class="form-control" id="title" name="title" value="<?= $post_data->title ?? "" ?>" placeholder="제목" required />  <!-- (3) -->
         <small id="titlehelp" class="form-text text-muted">제목은 4-100글자 사이입니다.</small>  <!-- (4) -->
     </div>
     <div class="form-group">
         <label for="content">내용을 입력하세요</label>
-        <textarea name="content" class="form-control" id="content" rows="10" required><?= $post_data['content'] ?? "" ?></textarea>  <!-- (5) -->
+        <textarea name="content" class="form-control" id="content" rows="10" required><?= $post_data->content ?? "" ?></textarea>  <!-- (5) -->
     </div>
-    <p style="text-align: right;">  <!-- (6) -->
-        <input type="submit" class="btn btn-primary" value="저장">  <!-- (7) -->
+    <p style="text-align: right;">
+        <input type="submit" class="btn btn-primary" value="저장">
         <a href="/post/" class="btn btn-info">취소</a>
     </p>
     <?php
