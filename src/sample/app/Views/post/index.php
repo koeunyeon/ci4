f3ad3acff6610fde75f1bb4348d5b0d2570db750
koeunyeon <koeunyeon@gmail.com>
2021-11-15 18:53:46 +0900

    설명을 위한 주석 삭제

--- src/sample/app/Views/post/index.php
+++ src/sample/app/Views/post/index.php
@@ -5,16 +5,16 @@ foreach($post_list as $post){
     ?>
     <div class="item mb-5">
         <div class="media">
-            <!-- <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="/assets/images/blog/blog-post-thumb-7.jpg" alt="image"> --> <!-- (1) -->
+            <!-- <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="/assets/images/blog/blog-post-thumb-7.jpg" alt="image"> -->
             <div class="media-body">
                 <h3 class="title mb-1">
-                    <a href="<?= site_url("/post/show/{$post['post_id']}") ?>"><?= $post['title'] ?></a> <!-- (2) -->
+                    <a href="<?= site_url("/post/show/{$post['post_id']}") ?>"><?= $post['title'] ?></a>
                 </h3>
                 <div class="meta mb-1">
-                    <span class="date"><?= $post['created_at'] ?></span> <!-- (3) -->
-                    <!-- <span class="comment"><a href="<?= site_url("/post/show/{$post['post_id']}#comment") ?>">4 comments</a></span></div> -->  <!-- (4) -->
-                <div class="intro"><?= $post['content'] ?></div>  <!-- (5) -->
-                <a class="more-link" href="<?= site_url("/post/show/{$post['post_id']}/#content") ?>">Read more &rarr;</a>  <!-- (6) -->
+                    <span class="date"><?= $post['created_at'] ?></span>
+                    <!-- <span class="comment"><a href="<?= site_url("/post/show/{$post['post_id']}#comment") ?>">4 comments</a></span></div> -->
+                <div class="intro"><?= $post['content'] ?></div>
+                <a class="more-link" href="<?= site_url("/post/show/{$post['post_id']}/#content") ?>">Read more &rarr;</a>
             </div><!--//media-body-->
         </div><!--//media-->
     </div><!--//item-->
