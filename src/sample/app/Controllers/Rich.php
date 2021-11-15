ffd12113c946ea25ee429d19107559aba84ceb71
koeunyeon <koeunyeon@gmail.com>
2021-11-15 18:42:11 +0900

    설명을 위한 주석 삭제

--- src/sample/app/Controllers/Rich.php
+++ src/sample/app/Controllers/Rich.php
@@ -10,8 +10,7 @@ class Rich extends BaseController
 {
     public function create()
     {
-        $richModel = new  RichModel();
-        // (1)
+        $richModel = new  RichModel();        
         $last_insert_id = $richModel->insert([
             'name' => 'rich',
             'age' => 22
@@ -21,44 +20,43 @@ class Rich extends BaseController
         return $this->response->setJSON($find_result);
     }
 
-    public function save($id, $name, $age) // (2)
+    public function save($id, $name, $age)
     {
         $richModel = new  RichModel();
         $richData = $richModel->find($id);
-        if ($richData === null) { // (3)
+        if ($richData === null) {
             $richData = [];
         }
-
-        // (4)
+        
         $richData['name'] = $name;
         $richData['age'] = $age;
         
-        $richModel->save($richData); // (5)
+        $richModel->save($richData);
         
-        $last_insert_id = $richModel->getInsertID(); // (6)
+        $last_insert_id = $richModel->getInsertID();
 
-        if ($last_insert_id == null) { // (7)
+        if ($last_insert_id == null) {
             $last_insert_id = $richData['sample_rich_id'];
         }
 
-        $last_insert_data = $richModel->find($last_insert_id); // (8)
+        $last_insert_data = $richModel->find($last_insert_id);
         return $this->response->setJSON($last_insert_data);
     }
 
     public function remove($id)
     {
         $richModel = new  RichModel();
-        $richModel->delete($id); // (9)
+        $richModel->delete($id);
 
-        $select_result_before = $richModel->find($id); // (10)
-        $with_deleted_result_before = $richModel->withDeleted()->find($id);  // (11)
+        $select_result_before = $richModel->find($id);
+        $with_deleted_result_before = $richModel->withDeleted()->find($id);
 
-        $richModel->purgeDeleted();  // (12)
+        $richModel->purgeDeleted();
 
-        $select_result_after = $richModel->find($id);  // (13)
-        $with_deleted_result_after = $richModel->withDeleted()->find($id);  // (14)
+        $select_result_after = $richModel->find($id);
+        $with_deleted_result_after = $richModel->withDeleted()->find($id);
 
-        return $this->response->setJSON([  // (15)
+        return $this->response->setJSON([
             'select_result_before' => $select_result_before,
             'with_deleted_result_before' => $with_deleted_result_before,
             'select_result_after' => $select_result_after,
