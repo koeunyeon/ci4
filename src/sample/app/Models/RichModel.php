bda797b251dcd97855ba23fa0fd3430c0bd755cd
koeunyeon <koeunyeon@gmail.com>
2021-11-15 18:41:06 +0900

    설명을 위한 주석 삭제

--- src/sample/app/Models/RichModel.php
+++ src/sample/app/Models/RichModel.php
@@ -12,6 +12,6 @@ class RichModel  extends Model
     protected $allowedFields = ['name','age'];
     protected $primaryKey = "sample_rich_id";
 
-    protected $useSoftDeletes = true; // (1)
-    protected $useTimestamps = true; // (2)
+    protected $useSoftDeletes = true;
+    protected $useTimestamps = true;
 }
